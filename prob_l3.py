707359ee48d8df5d49bd6b56ba48b70fd3702ba5
Shinji Kato <hole.in1.kato@gmail.com>
2017-09-11 10:42:44 +0630

    Change variable names

--- prob_l3.py
+++ prob_l3.py
@@ -46,36 +46,36 @@ def num_digit(n):
     return digit2_count, digit1_count
 
 
-def mix_3str(str1, str2, str3, bitmap):
+def mix_3str(str0, str1, str2, bitmap):
     '''
-        str1: Name of first person
-        str2: Name of second person
-        str3: Name of third person
-        bitmap: Tenary Integer, 2: str1, 1: str2, 0: str3
+        str0: Name of first person
+        str1: Name of second person
+        str2: Name of third person
+        bitmap: Tenary Integer, 2: str0, 1: str1, 0: str2
         Return: Combined string
     '''
 
     mixed_str = ''
+    str0_pos = 0
     str1_pos = 0
     str2_pos = 0
-    str3_pos = 0
 
     while bitmap > 0:
         digit = bitmap % 3
         if digit == 2:
-            mixed_str += str1[str1_pos]
-            str1_pos += 1
-        elif digit == 1:
             mixed_str += str2[str2_pos]
             str2_pos += 1
+        elif digit == 1:
+            mixed_str += str1[str1_pos]
+            str1_pos += 1
         else:
-            mixed_str += str3[str3_pos]
-            str3_pos += 1
+            mixed_str += str0[str0_pos]
+            str0_pos += 1
         bitmap //= 3
 
-    while str3_pos < len(str3):
-        mixed_str += str3[str3_pos]
-        str3_pos += 1
+    while str0_pos < len(str0):
+        mixed_str += str0[str0_pos]
+        str0_pos += 1
 
     return  mixed_str
 
@@ -91,18 +91,18 @@ def solve(tc):
     total_pw = 0
     new_pw = dict()
 
-    name1 = tc.names[0]
-    name2 = tc.names[1]
-    name3 = tc.names[2]
+    name0 = tc.names[0]
+    name1 = tc.names[1]
+    name2 = tc.names[2]
+    len_name0 = len(name0)
     len_name1 = len(name1)
     len_name2 = len(name2)
-    len_name3 = len(name3)
 
-    for i in range(3**(len_name1+len_name2+len_name3)):
+    for i in range(3**(len_name0+len_name1+len_name2)):
         num_digit2, num_digit1 = num_digit(i)
-        if num_digit2 != len_name1: continue
-        if num_digit1 != len_name2: continue
-        mixed_str = mix_3str(name1, name2, name3, i)
+        if num_digit2 != len_name2: continue
+        if num_digit1 != len_name1: continue
+        mixed_str = mix_3str(name0, name1, name2, i)
         if mixed_str not in new_pw:
             new_pw[mixed_str] = True
             print(mixed_str)
